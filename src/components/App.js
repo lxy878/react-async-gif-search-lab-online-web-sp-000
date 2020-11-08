f16d5cb9d2a8787b64d695ac6d4696be81e5e8b7
Xiaoyi Liu <lxy878@gmail.com>
2020-11-08 16:17:40 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import NavBar from './NavBar'
+import GifListContainer from '../containers/GifListContainer.js'
 
 const App = () => {
   return (
     <div>
         < NavBar color='black' title="Giphy Search" />
+        <GifListContainer/>
     </div>
   )
 }
